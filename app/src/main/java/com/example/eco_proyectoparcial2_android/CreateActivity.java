0db99bfe90935c6be4ee21271fa76b33bce21865
erika2201 <erikapapamija@gmail.com>
2021-11-21 15:42:20 -0500

    Falta una pantalla

--- app/src/main/java/com/example/eco_proyectoparcial2_android/CreateActivity.java
+++ app/src/main/java/com/example/eco_proyectoparcial2_android/CreateActivity.java
@@ -2,13 +2,26 @@ package com.example.eco_proyectoparcial2_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CreateActivity extends AppCompatActivity {
+
+    private Button createBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        createBtn = findViewById(R.id.createBtn);
+
+        //De Create a ListaAhorros
+        createBtn.setOnClickListener(
+                (v) ->{
+                    Intent i = new Intent(this, MySavingActivity.class);
+                    startActivity(i);
+                });
     }
 }
---
+++ app/src/main/java/com/example/eco_proyectoparcial2_android/MySavingActivity.java
@@ -0,0 +1,14 @@
+package com.example.eco_proyectoparcial2_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MySavingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_saving);
+    }
+}
